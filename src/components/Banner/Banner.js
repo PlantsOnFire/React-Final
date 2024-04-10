8ffeecb523d2479f93772d7e66fec15e52a92ffa
Ashley Owen <ashleyaowen@hotmail.com>
2024-04-10 17:56:17 -0400

    Update Banner.js

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -2,7 +2,7 @@ import './Banner.scss';
 
 function Banner ({img, img2, title}) {
   return(
-    <div className='banner row justify-content-center align-items-center'>
+    <div className='banner row justify-content-center align-items-center m-0'>
       <div className='col-md-1'></div>
       <img src={img} className='banner-img col-md-2 text-center'/>
       <div className='col-md text-center'><h1>{title}</h1></div>
