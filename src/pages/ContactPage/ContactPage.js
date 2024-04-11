62520e6f7e5613d914e66007377e3be328df2e1d
info3170-Christine <cbottone88@gmail.com>
2024-04-11 19:22:13 -0400

    Added all the information to pages

--- src/pages/ContactPage/ContactPage.js
+++ src/pages/ContactPage/ContactPage.js
@@ -1,9 +1,17 @@
 import './ContactPage.scss';
+import Banner from '../../components/Banner/Banner';
+import ContactForm from '../../components/ContactForm/ContactForm';
 
 function ContactPage () {
   return(
     <main>
-      CONTACT PAGE
+      <Banner title='CONTACT US'/>
+      <div className='topText'>
+        Having some troubles or want to know more? Send us a message!
+      </div>
+      <div className='contactForm'>
+        <ContactForm/>
+      </div>
     </main>
   )
 }
